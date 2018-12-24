6f048f217239c6991ce384e23a192bb1d76b4d6e
lishang6257 <lishang6257@163.com>
2018-12-24 23:54:01 +0800

    v1.0.0

---
+++ main.m
@@ -0,0 +1,141 @@
+close all;
+% clear all;
+
+[a b] = imread('fly.gif');
+if ~isempty(b)
+    fly = ind2rgb(a,b);
+end
+
+img1_gray=rgb2gray(fly);
+imwrite(img1_gray,'fly.jpg','quality',100);
+fly_point = match('fly.jpg','fly.jpg');
+
+
+imgOutVert = 'imgOutVert.png';
+imgOutHori = 'imgOutHori.png';
+matchings = 'matchings.txt';
+keys1 = 'keys1.txt';
+keys2 = 'keys2.txt';
+file_img1 = 'fly.jpg';
+file_img2 = 'region.jpg';
+flag_resize = 0;
+
+
+
+
+Start = 1;
+End = 4;
+for name = Start : End
+    %%%%%%%%%% step 0 读取图片 %%%%%%%%%%%%%%%%%%%%
+    imgName1 = ['test' num2str(name) '-1.jpg'];
+    imgName2 = ['test' num2str(name) '-2.jpg'];
+    img1 = imread(imgName1);
+    img2 = imread(imgName2);
+    img1_gray = rgb2gray(img1);
+    img2_gray = rgb2gray(img2);
+
+    moving = img2_gray;
+    fixed = img1_gray;
+    
+%     figure(name+30)
+%     figure, imshowpair(img1, img2, 'montage');
+    
+    %%%%%%%%%% step 1 初始配准（粗配准） %%%%%%%%%%%%%%%%%%%%
+    [optimizer, metric] = imregconfig('multimodal');
+%     % 有两种选择‘monomodal’, 'multimodal'两种，分别质量两幅图像是单一模态还是多模态，根据需要自己选择。
+    
+    %%%%%%%%%%% step 2  优化 %%%%%%%%%%%%%%
+    % disp(optimizer);
+    % disp(metric);
+    
+    %%%% 2.1 改变优化器的步长已达到对更加精细的变换。
+    optimizer.InitialRadius = optimizer.InitialRadius/3.5;
+    
+    %%%% 2.2 （2.1）基础上改变最大迭代次数
+    optimizer.MaximumIterations = 400;
+    
+    %%%% 3 初始条件
+    tformSimilarity = imregtform(moving,fixed,'similarity',optimizer,metric);
+    
+%     %%%% 4 精对准
+    tform = imregtform(moving,fixed,'affine',optimizer,metric,...
+        'InitialTransformation',tformSimilarity);
+    
+    Rmoving = imref2d(size(moving));
+    Rfixed = imref2d(size(fixed));
+    [movingReg_rgb] = imwarp(img2,Rmoving,tform,'OutputView',Rfixed, 'SmoothEdges', true);
+    movingRegX = rgb2gray(movingReg_rgb);
+
+    
+    % 转lab空间
+    img_sub1_lab = rgb2lab(movingReg_rgb);
+    img_sub2_lab = rgb2lab(img1);
+    % 计算色差（色彩相似度）
+    img_deltaE = sqrt( sum( (img_sub1_lab - img_sub2_lab).^2 , 3));
+
+    
+    threshold = 25;
+    img_deltaE_bw = ones(size(img_deltaE));
+    img_deltaE_bw(img_deltaE <= threshold) = 0;
+    img_deltaE_bw(img_deltaE > threshold) = 1;
+    img_deltaE_bw = logical(img_deltaE_bw);
+
+   
+    
+    img_sub1 = imsubtract(fixed,movingRegX);
+    img_sub2 = imsubtract(movingRegX,fixed);
+    img_sub = img_sub1 + img_sub2;
+    res1 = medfilt2(img_sub);
+    if name == 1
+        res = HomoFilter(res1, 0.05 ,0.1 , 500 , 200 );
+    elseif name == 2
+        res = HomoFilter(res1, 0.4 ,0.9 , 500 , 200 );
+    elseif name == 3
+        res = HomoFilter(res1, 0.7 ,0.4 , 500 , 200 );
+    elseif name == 4
+        res = HomoFilter(res1, 1 ,1, 500 , 200 );
+    end
+    t = graythresh(res);
+    res = im2bw( real(res),t);
+    
+
+    res2 = imreconstruct(res,img_deltaE_bw);
+    res = max(res2,res);
+    
+    res = imclose(res,strel('square',3));
+    res = imopen(res,strel('square',3));
+    res = bwfill(res,'holes');
+    res = medfilt2(res);
+    
+    dis = 4;
+    res(1:1+dis,:) = 0;
+    res(:,1:1+dis) = 0;
+    res(size(img1,1)-dis:size(img1,1),:) = 0;
+    res(:,size(img1,2)-dis:size(img1,2)) = 0;
+    
+    
+%     figure(name+10)
+%     subplot(221),imshowpair(fixed, movingRegX);
+%     subplot(222),imshow(img_sub1+img_sub2);
+%     subplot(223),imshow(res);
+%     subplot(224),imshow(img_deltaE_bw);
+    
+    figure(name+20)
+    subplot(121)
+    imshow(img1);
+    hold on;
+    region = regionprops(bwlabel(res),'Boundingbox');
+    for i = 1 : size(region,1)
+        rec = floor(region(i).BoundingBox);
+        imwrite(  res( rec(2):rec(2)+rec(4) , rec(1):rec(1)+rec(3) ) ,'region.jpg','quality',100);
+        p = match('region.jpg','fly.jpg');
+        if p > 0
+            rectangle('Position',region(i).BoundingBox,'Curvature',[0,0],'LineWidth',3 ,'LineStyle','-','EdgeColor','b');
+        else
+            rectangle('Position',region(i).BoundingBox,'Curvature',[0,0],'LineWidth',3 ,'LineStyle','-','EdgeColor','r');
+        end
+    end
+    hold off
+    subplot(122),imshow(img2);
+    
+end
---
+++ matchDir.m
@@ -0,0 +1,113 @@
+% num = match(image1, image2)
+%
+% This function reads two images, finds their SIFT features, and
+%   displays lines connecting the matched keypoints.  A match is accepted
+%   only if its distance is less than distRatio times the distance to the
+%   second closest match.
+% It returns the number of matches displayed.
+%
+% Example: match('scene.pgm','book.pgm');
+
+function [p] = matchDir(image1, image2)% image1 = 'v1.jpg';image2 = 'v2.jpg';
+% Find SIFT keypoints for each image
+[im1, des1, loc1] = sift(image1);
+[im2, des2, loc2] = sift(image2);
+
+% For efficiency in Matlab, it is cheaper to compute dot products between
+%  unit vectors rather than Euclidean distances.  Note that the ratio of 
+%  angles (acos of dot products of unit vectors) is a close approximation
+%  to the ratio of Euclidean distances for small angles.
+%
+% distRatio: Only keep matches in which the ratio of vector angles from the
+%   nearest to second nearest neighbor is less than distRatio.
+distRatio = 0.6;   
+
+% For each descriptor in the first image, select its match to second image.
+des2t = des2';                          % Precompute matrix transpose
+
+match = ones(1, max( size(loc1,1) , size(loc2,1) ) );
+for i = 1 : size(des1,1)
+   dotprods = des1(i,:) * des2t;        % Computes vector of dot products
+   [vals,indx] = sort(acos(dotprods));  % Take inverse cosine and sort results
+
+   % Check if nearest neighbor has angle less than distRatio times 2nd.
+   if (vals(1) < distRatio * vals(2))
+      match(i) = indx(1);
+   else
+      match(i) = 0;
+   end
+end
+
+% Create a new image showing the two images side by side.
+% Show a figure with lines joining the accepted matches.
+
+
+% im3 = appendimages(im1,im2);
+% figure('Position', [100 100 size(im3,2) size(im3,1)]);
+% colormap('gray');
+% imagesc(im3);
+% hold on;
+% cols1 = size(im1,2);
+
+
+num = 0;
+sum_a = 0;
+sum_b = 0;
+res = zeros(sum(match > 0),5);
+for i = 1: size(des1,1)
+  if (match(i) > 0)
+      
+%     line([loc1(i,2) loc2(match(i),2)+cols1], ...
+%          [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
+     
+    a = loc2(match(i),2) - loc1(i,2);
+    b = loc2(match(i),1) - loc1(i,1);
+    scale = loc2(match(i),3) - loc1(i,3);
+    dir = loc2(match(i),4) - loc1(i,4);
+    num = num +1;
+    res(num,:) = [i a  b  scale dir];
+  end
+end
+
+hold off;
+
+% 奇异点剔除[正态分布]
+res_std = std( res(:,2:5) );
+res_mean = mean( res(:,2:5) );
+res_new = res;
+num = 0;
+for i = 1 : size(res,1)
+    if  ( res(i,2) < (res_mean(1) + 3*res_std(1)) && res(i,2) > (res_mean(1) - 3*res_std(1)) ) ...
+        && ( res(i,3) < (res_mean(2) + 3*res_std(2)) && res(i,3) > (res_mean(2) - 3*res_std(2)) ) ...
+        && ( res(i,4) < (res_mean(3) + 3*res_std(3)) && res(i,4) > (res_mean(3) - 3*res_std(3)) ) ...
+        && ( res(i,5) < (res_mean(4) + 3*res_std(4)) && res(i,5) > (res_mean(4) - 3*res_std(4)) ) %...
+%         && abs(res(i,2)) < 30 && abs(res(i,3)) < 30 && abs(res(i,4)) < 0.2
+    % 满足3-sigma法则
+        num = num + 1;
+        res_new(num,:) = res(i,:);
+%         select = res(i,:)
+    end
+end
+res_new = res_new(1 : num,:);
+
+% for j = 1: size(res_new,1)
+%     i = res_new(j,1);
+%     if i < 1
+%         continue;
+%     end
+% %     [loc1(i,2) loc2(match(i),2)+cols1]
+% %          [loc1(i,1) loc2(match(i),1)]
+%     line([loc1(i,2) loc2(match(i),2)+cols1], ...
+%          [loc1(i,1) loc2(match(i),1)], 'Color', 'c');
+% end
+% hold off;
+res_new_mean = mean(res_new(:,2:4));
+p = res_new_mean(1:2);
+fprintf('Found (%d , %d )\n', p);
+% p = round(p);
+
+% fprintf('Found %d matches.\n', num);
+
+
+
+
